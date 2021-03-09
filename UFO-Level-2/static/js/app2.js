9f53fb8456da932614552271ccfbe500f8e4fda2
Ciara Spencer <ciara.spencer314@gmail.com>
2021-03-09 13:25:35 -0500

    updating UFO Level 2 with filter values

--- UFO-Level-2/static/js/app2.js
+++ UFO-Level-2/static/js/app2.js
@@ -27,49 +27,74 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 //create event handlers
-button.on("click", runEnter);
-form.on("submit", runEnter);
+// button.on("click", runEnter);
+// form.on("submit", runEnter);
 
-// complete event handler function for the form
-function runEnter() {
+d3.selectAll("input").on("change", updateFilters);
 
-    // prevent the page from refreshing
-    d3.event.preventDefault();
+var filters = {};
 
- // select the input element and get the raw html node // select the value property of the input element
- var datetime_input = d3.select("#datetime").property("value");
- var city_input = d3.select("#city").property("value");
- var state_input = d3.select("#state").property("value");
- var country_input = d3.select("#country").property("value");
- var shape_input = d3.select("#shape").property("value");
- 
- // log input date
- console.log(datetime_input);
+function updateFilters() {
 
- //filtered data
- if (datetime_input) {
- var filteredData = data.filter(dates => dates.datetime === datetime_input); }
- if (city_input) {
- var filteredData = data.filter(cities => cities.city === city_input); }
- if (state_input) {
- var filteredData = data.filter(states => states.state === state_input); }
- if (country_input) {
- var filteredData = data.filter(countries => countries.country === country_input); }
- if (shape_input) {
- var filteredData = data.filter(shapes => shapes.shape === shape_input); }
+    let changedElement = d3.select(this);
+    let elementValue = changedElement.property("value");
+    let filterId = changedElement.attr("id");
 
+    if(elementValue) {
+        filters[filterId] = elementValue
+    } else {
+        delete filters[filterId]
+    }
 
-    //log filtered values
-    console.log(filteredData);
+    filterData();
+}
 
-    //filtered data tables
+function filterData() {
+    let filteredData = tableData;
+    Object.entries(filters).forEach(([k, v]) => {
+        filteredData = filteredData.filter(row => row[k] === v)
+    });
     fillTable(filteredData);
- };
+}
+
+// // complete event handler function for the form
+// function runEnter() {
+
+//     // prevent the page from refreshing
+//     d3.event.preventDefault();
+
+//  // select the input element and get the raw html node // select the value property of the input element
+//  var datetime_input = d3.select("#datetime").property("value");
+//  var city_input = d3.select("#city").property("value");
+//  var state_input = d3.select("#state").property("value");
+//  var country_input = d3.select("#country").property("value");
+//  var shape_input = d3.select("#shape").property("value");
+ 
+//  // log input date
+//  console.log(datetime_input);
+
+//  //filtered data
+//  if (datetime_input) {
+//  var filteredData = data.filter(dates => dates.datetime === datetime_input); }
+//  if (city_input) {
+//  var filteredData = data.filter(cities => cities.city === city_input); }
+//  if (state_input) {
+//  var filteredData = data.filter(states => states.state === state_input); }
+//  if (country_input) {
+//  var filteredData = data.filter(countries => countries.country === country_input); }
+//  if (shape_input) {
+//  var filteredData = data.filter(shapes => shapes.shape === shape_input); }
+
+
+//     //log filtered values
+//     console.log(filteredData);
+
+//     //filtered data tables
+//     fillTable(filteredData);
+//  };
 
 
   
-
-
 // var dropDown = d3.select("tbody").append("select")
 //                     .attr("name", "shape-list");
                 
