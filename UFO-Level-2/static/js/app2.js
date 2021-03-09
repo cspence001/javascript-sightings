2e363856802f9a0a323080dc59e2cf112e03947a
Ciara Spencer <ciara.spencer314@gmail.com>
2021-03-09 14:11:12 -0500

    updating UFO-Level-2 .js

--- UFO-Level-2/static/js/app2.js
+++ UFO-Level-2/static/js/app2.js
@@ -57,43 +57,6 @@ function filterData() {
     fillTable(filteredData);
 }
 
-// // complete event handler function for the form
-// function runEnter() {
-
-//     // prevent the page from refreshing
-//     d3.event.preventDefault();
-
-//  // select the input element and get the raw html node // select the value property of the input element
-//  var datetime_input = d3.select("#datetime").property("value");
-//  var city_input = d3.select("#city").property("value");
-//  var state_input = d3.select("#state").property("value");
-//  var country_input = d3.select("#country").property("value");
-//  var shape_input = d3.select("#shape").property("value");
- 
-//  // log input date
-//  console.log(datetime_input);
-
-//  //filtered data
-//  if (datetime_input) {
-//  var filteredData = data.filter(dates => dates.datetime === datetime_input); }
-//  if (city_input) {
-//  var filteredData = data.filter(cities => cities.city === city_input); }
-//  if (state_input) {
-//  var filteredData = data.filter(states => states.state === state_input); }
-//  if (country_input) {
-//  var filteredData = data.filter(countries => countries.country === country_input); }
-//  if (shape_input) {
-//  var filteredData = data.filter(shapes => shapes.shape === shape_input); }
-
-
-//     //log filtered values
-//     console.log(filteredData);
-
-//     //filtered data tables
-//     fillTable(filteredData);
-//  };
-
-
 
 // //combine city, states for address param
 // let address = data.map(function(location) {
